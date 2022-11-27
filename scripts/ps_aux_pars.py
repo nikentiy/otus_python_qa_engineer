c60e17441cbe3c3df9f751ef1e790800533ba6fa
Veronika Stepurova <vstepurova@hystax.com>
2022-11-27 18:43:56 +0200

    changes according comments from the review

--- scripts/ps_aux_pars.py
+++ scripts/ps_aux_pars.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 from functools import reduce
 from subprocess import (
-    run, PIPE, Popen
+    PIPE, Popen
 )
-from unicodedata import decimal
 
 
 def collect_data() -> list:
@@ -28,12 +27,12 @@ def number_active_processes(username: str, raw_data: list) -> int:
 
 
 def biggest_cpu_consumer(raw_data: list) -> str:
-    proc = reduce(lambda x, y: y if x.get("%CPU") < y.get("%CPU") else x, raw_data)
+    proc = reduce(lambda x, y: y if float(x.get("%CPU")) < float(y.get("%CPU")) else x, raw_data)
     return proc.get("COMMAND")[:20]
 
 
 def biggest_memory_consumer(raw_data: list) -> str:
-    proc = reduce(lambda x, y: y if x.get("%MEM") < y.get("%MEM") else x, raw_data)
+    proc = reduce(lambda x, y: y if float(x.get("%MEM")) < float(y.get("%MEM")) else x, raw_data)
     return proc.get("COMMAND")[:20]
 
 
@@ -55,16 +54,19 @@ def collect_result():
     date = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
     raw_data = collect_data()
     users = user_list(raw_data)
+    result = f"Users: {users}\n" \
+             f"Total number of processes: {len(raw_data)}\n" \
+             f"Total memory usage: {total_memory_usage(raw_data)}\n" \
+             f"Total CPU usage: {total_cpu_usage(raw_data)}\n" \
+             f"The biggest memory consumer: %{biggest_memory_consumer(raw_data)}\n" \
+             f"The biggest CPU consumer: %{biggest_cpu_consumer(raw_data)}\n" \
+             f"User's processes:\n"
+    for user in users:
+        user_proc = f"{user}: {number_active_processes(user, raw_data)}\n"
+        result = result + user_proc
     with open(f'./src/{date}_system_states.txt', "w+") as f:
-        f.write(f"Users: {users}\n")
-        f.write(f"Total number of processes: {len(raw_data)}\n")
-        f.write("User's processes:\n")
-        for user in users:
-            f.write(f"{user}: {number_active_processes(user, raw_data)}\n")
-        f.write(f"Total memory usage: {total_memory_usage(raw_data)}\n")
-        f.write(f"Total CPU usage: {total_cpu_usage(raw_data)}\n")
-        f.write(f"Biggest memory consumer: %{biggest_memory_consumer(raw_data)}\n")
-        f.write(f"Biggest CPU consumer: %{biggest_cpu_consumer(raw_data)}\n")
+        f.write(result)
+        print(result)
 
 
 if __name__ == '__main__':
